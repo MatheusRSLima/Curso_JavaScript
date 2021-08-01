a9c71615a03af3fca181ea65b66eb1d361d3316e
MatheusRSLima <68763902+MatheusRSLima@users.noreply.github.com>
2021-08-01 16:41:33 -0300

    primeiro dia de cruso

---
+++ aula-03/ex/index.js
@@ -0,0 +1,1 @@
+console.log(`Meu nome é "Matheus Lima". Estou aprendendo JavaScript às 10 manhã`);
---
+++ aula-03/index.js
@@ -0,0 +1,5 @@
+console.log("Luiz 'Otávio'"); //string = Texto
+console.log('Luiz "Otávio"'); 
+console.log(`'Luiz' "Otávio"`);
+
+console.log(123456, 15.85, 'Luiz Otávio De Novo');
