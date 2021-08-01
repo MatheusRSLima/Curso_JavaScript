60eac62da49ac16c207c54b42d43739fdd34d0f6
MatheusRSLima <68763902+MatheusRSLima@users.noreply.github.com>
2021-08-01 17:10:04 -0300

    finalizando o primeiro dia

---
+++ aula-07/index.js
@@ -0,0 +1,22 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisa ter nomes significativos
+// Não pode começar o nome de uma variável com número
+// Não podem conter espaços ou traços
+// Utilizamos CamelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+/* const nome = 'João';
+nome = 'Otávio'; */
+
+//String = Text | Number = Número
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoDuplicado);
+
+/* console.log(typeof primeiroNumero); */
